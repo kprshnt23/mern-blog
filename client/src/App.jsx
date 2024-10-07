cf3bdba169af975aade1b94800d0118ee198887c
Prashanth <prashanth@Prashanths-MacBook-Pro.local>
2024-10-07 11:43:31 +0530

    Mern starter

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, About, Dashboard, Projects, SignIn, Signup } from "./pages";
+import Header from "./components/Header";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import Home from "./Home";
+import About from "./About";
+import Dashboard from "./Dashboard";
+import Projects from "./Projects";
+import SignIn from "./SignIn";
+import Signup from "./Signup";
+
+export { Home, About, Dashboard, Projects, SignIn, Signup };
